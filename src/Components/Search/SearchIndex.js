140d087498275ad0c1f719422e551535702b1f56
Josh Geddes <josh.geddes96@gmail.com>
2021-10-19 18:41:01 -0400

    finished all challenges

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import {Input} from 'reactstrap';
  
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
+class SearchIndex extends React.Component {
+  constructor(props) {
+    super(props);
 
- function searchFunction() {
- }
+    this.state = {
+      search: '',
+      things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards'],
+      results: []
+    }
+  }
+  
 
+ searchFunction = () => {
+    this.setState(prevState => ({results: prevState.things.filter(thing => thing === this.state.search || this.state.search === '')}))
+ }
  render() {
+   return (
      <div>
-       <Input placeholder='Search Here' />
+       <Input placeholder='Search Here' value={this.state.search} onChange={e => this.setState({search: e.target.value})}/>
+       <button onClick={this.searchFunction}>Search</button>
        <h3>Results:</h3>
+       {this.state.results.map(result => (<p key={result}>{result}</p>))}
      </div>
+   );
  }
+}
 
  
-export SearchIndex;
+export default SearchIndex;
---
+++ src/Components/ToDo/ToDoList.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class ToDoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
+        this.newTodo = React.createRef();
+    }
+    handleAddTodo = e => {
+        const name = this.newTodo.current.value;
+        if(name === '') return;
+        this.setState(prevState => ({todos: [...prevState.todos, {name: name, completed: false }]}))
+    }
+    handleToggle = todo => {
+        this.setState(prevState => ({todos: [...prevState.todos.filter(curr => curr.name !== todo.name), {...todo, completed: !todo.completed}]}))
+    }
+    removeCompletedTodos = () => {
+        this.setState(prevState => ({todos: prevState.todos.filter(curr => !curr.completed)}))
+    }
+    render() {
+        console.log(this.state.todos)
+        return (
+            <div>
+                <div>
+                    <input type='text' ref={this.newTodo} />
+                    <button onClick={this.handleAddTodo}>Add ToDo</button>
+                    <button onClick={this.removeCompletedTodos}>Remove Complete</button>
+                </div>
+                <div>
+                    {
+                        this.state.todos.map(todo => (
+                            <div key={todo.name}>
+                                <label>
+                                    <input type='checkbox' checked={todo.complete} onChange={() => this.handleToggle(todo)}/>
+                                    {todo.name}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ToDoList;
